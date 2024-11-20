3498b7c2737814f33337b77c70af4dd9c2aec19e
Samuel McBroom <sam@sammcb.com>
2024-11-20 15:05:58 -0800

    Merge resourcepack repo.

--- do.sh
+++ do.sh
@@ -3,7 +3,9 @@
 VERSION=1.0.1
 MINECRAFT_VERSION=1.21.3
 
-ZIP_NAME="elytra-chestplates-${VERSION}-${MINECRAFT_VERSION}.zip"
+DATAPACK_ZIP_NAME="elytra-chestplates-${VERSION}-${MINECRAFT_VERSION}.zip"
+RESOURCEPACK_ZIP_NAME="Elytra Chestplates ${VERSION} ${MINECRAFT_VERSION}.zip"
+BUILD_DIRECTORY="build"
 
 print_info() {
 	printf "\e[1;35m$1\e[0m - \e[0;37m$2\e[0m\n"
@@ -11,20 +13,25 @@ print_info() {
 
 help() {
 	print_info help "Display callable targets"
-	print_info build "Create a datapack zip file"
+	print_info build "Create a datapack and resourcepack zip file"
 	print_info clean "Remove build artifacts"
 }
 
 build() {
-	local root_directory="datapack"
+	local datapack_root_directory="datapack"
+	local resourcepack_root_directory="resourcepack"
 	clean
-	cd ${root_directory}
-	zip -q -r "../${ZIP_NAME}" data pack.mcmeta pack.png
+	mkdir ${BUILD_DIRECTORY}
+	cd ${datapack_root_directory}
+	zip -q -r "../${BUILD_DIRECTORY}/${DATAPACK_ZIP_NAME}" data pack.mcmeta pack.png
+	cd ..
+	cd ${resourcepack_root_directory}
+	zip -q -r "../${BUILD_DIRECTORY}/${RESOURCEPACK_ZIP_NAME}" assets pack.mcmeta pack.png
 	cd ..
 }
 
 clean() {
-	rm -f ${ZIP_NAME}
+	rm -rf ${BUILD_DIRECTORY}
 }
 
 if [ ${1:+x} ]; then
